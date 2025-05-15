62fd60dabe10e8676099ee58ddfdf3739633488c
Jongchoon Na <gppc5096@gmail.com>
2025-05-15 10:45:17 +0900

    feat: favicon(가계부_icon.ico) 글로벌 적용 (head.tsx)

---
+++ src/app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/가계부_icon.ico" type="image/x-icon" />
+      {/* <link rel="icon" href="/가계부_icon.png" type="image/png" /> */}
+    </>
+  );
+} 
